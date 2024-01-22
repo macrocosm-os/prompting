d1649b5f3cff28bd29eef7f1d9648e00d3771e67
mccrindlebrian <mccrinbc@gmail.com>
2024-01-22 12:25:45 -0800

    check to see if device startswith cuda, so we allow arbitrary cuda devices

--- prompting/rewards/relevance.py
+++ prompting/rewards/relevance.py
@@ -21,12 +21,10 @@ class RelevanceRewardModel(BaseRewardModel):
         self.model = AnglE.from_pretrained(
             "WhereIsAI/UAE-Large-V1", pooling_strategy=pooling_strategy
         )
-        if device == "cuda":
+        if device.startswith("cuda"):
             self.model = self.model.cuda()
 
-    def reward(
-        self, reference: str, completions: List[str]
-    ) -> BatchRewardOutput:
+    def reward(self, reference: str, completions: List[str]) -> BatchRewardOutput:
         """Calculates the cosine similarity between sentence embeddings of the reference and completions.
         We subtract a baseline score which is what an empty string would get (a failed completion). This is usually around 0.35
         We also clip the rewards between 0 and 1. The maximum effective score is around 0.65
@@ -35,14 +33,22 @@ class RelevanceRewardModel(BaseRewardModel):
         rewards = []
         timings = []
         # baseline is the cosine similarity between the reference and an empty string
-        baseline = cosine_similarity(reference_embedding.reshape(1, -1), self.model.encode('', to_numpy=False).reshape(1, -1))
+        baseline = cosine_similarity(
+            reference_embedding.reshape(1, -1),
+            self.model.encode("", to_numpy=False).reshape(1, -1),
+        )
 
         for comp in completions:
             t0 = time.time()
 
             emb = self.model.encode(comp, to_numpy=False)
             # Calculate cosine similarity between reference and completion embeddings, and subtract baseline
-            score = cosine_similarity(reference_embedding.reshape(1, -1), emb.reshape(1, -1)) - baseline
+            score = (
+                cosine_similarity(
+                    reference_embedding.reshape(1, -1), emb.reshape(1, -1)
+                )
+                - baseline
+            )
 
             rewards.append(score)
             timings.append(time.time() - t0)
