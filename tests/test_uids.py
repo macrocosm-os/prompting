2e999de96a30b875939fae4096793f868059d4b0
Steffen Cruz <steffenjcruz@gmail.com>
2024-02-07 10:46:51 -0600

    Update test

--- tests/test_uids.py
+++ tests/test_uids.py
@@ -1,21 +1,22 @@
 
 import torch
+import random
 import pytest
 from types import SimpleNamespace
 from prompting.utils.uids import get_random_uids
 
-
-def make_mock_neuron(unique_coldkeys=False, unique_ips=False, vpermit_tao_limit=1000):
+def make_mock_neuron(sample_size, unique_coldkey_prob=False, unique_ip_prob=False, vpermit_tao_limit=1000):
 
     axons = [
         SimpleNamespace(coldkey="a", ip="0.0.0.1", is_serving=True),
         SimpleNamespace(coldkey="a", ip="0.0.0.0", is_serving=True),
         SimpleNamespace(coldkey="b", ip="0.0.0.1", is_serving=True),
         SimpleNamespace(coldkey="b", ip="0.0.0.0", is_serving=True),
-        SimpleNamespace(coldkey="c", ip="0.0.0.2", is_serving=True),
+        SimpleNamespace(coldkey="c", ip="0.0.0.2", is_serving=True), # validator
     ]
     metagraph = SimpleNamespace(
         axons = axons,
+        coldkeys = [axon.coldkey for axon in axons],
         validator_permit = torch.ones(len(axons), dtype=torch.bool),
         S = torch.zeros(len(axons)),
         n = torch.tensor(len(axons))
@@ -25,9 +26,10 @@ def make_mock_neuron(unique_coldkeys=False, unique_ips=False, vpermit_tao_limit=
         uid = 4,
         config = SimpleNamespace(
             neuron = SimpleNamespace(
+                sample_size = sample_size,
                 vpermit_tao_limit = vpermit_tao_limit,
-                query_unique_coldkeys = unique_coldkeys,
-                query_unique_ips = unique_ips,
+                unique_coldkey_prob = unique_coldkey_prob,
+                unique_ip_prob = unique_ip_prob,
             )
         ),
         metagraph = metagraph
@@ -42,7 +44,7 @@ def make_mock_neuron(unique_coldkeys=False, unique_ips=False, vpermit_tao_limit=
         ])
 def test_get_random_uids(unique_coldkeys, unique_ips, k, expected_result):
 
-    mock_neuron = make_mock_neuron(unique_coldkeys, unique_ips)
+    mock_neuron = make_mock_neuron(k, unique_coldkeys, unique_ips)
 
-    assert sorted(get_random_uids(mock_neuron, k).tolist()) == expected_result, "Incorrect uids returned."
+    assert sorted(get_random_uids(mock_neuron).tolist()) == expected_result, "Incorrect uids returned."
 
