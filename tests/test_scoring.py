ef1ec4f7bd040c887b421900e3165f98372fbcff
bkb2135 <98138173+bkb2135@users.noreply.github.com>
2024-02-08 12:15:16 -0500

    Update tests/test_scoring.py
    Co-authored-by: Steffen Cruz <steffenjcruz@gmail.com>

--- tests/test_scoring.py
+++ tests/test_scoring.py
@@ -16,7 +16,7 @@ dates3 = [date3.strftime(format) for format in date_formats]
 score3 = 0.0
 dates4 = [date4.strftime(format) for format in date_formats]
 score4 = 0.38251018447178037
-tuples = list(zip(dates1+dates2+dates3+dates4,[score1]*len(dates1)+[score2]*len(dates2)+[score3]*len(dates3)+[score4]*len(dates4)))
+tuples = list( zip(dates1+dates2+dates3+dates4, scores1+scores2+scores3+scores4) )
 
 @pytest.mark.parametrize('reference', dates1)
 @pytest.mark.parametrize('completion, expected_result', tuples)
