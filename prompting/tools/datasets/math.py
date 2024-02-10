4ee9636e029b70158e5c73e9f7268355d198d760
Steffen Cruz <steffenjcruz@gmail.com>
2024-02-09 18:26:01 -0600

    Add tags

--- prompting/tools/datasets/math.py
+++ prompting/tools/datasets/math.py
@@ -69,6 +69,7 @@ class MathDataset(Dataset):
             'content': info['problem'], # problem statement
             'internal_links': [info['topic'], info['subtopic']], # internal links
             'external_links': external_links,
+            "tags": info['forward_words'],
             'source': 'Mathgenerator',
             'extra': {'reward_type': info['reward_type'], 'solution': info['solution']}
         }
