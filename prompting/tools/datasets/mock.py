063288d85cb611bd7b321e920a1e0991e49aca3d
Steffen Cruz <steffenjcruz@gmail.com>
2024-02-09 18:25:29 -0600

    Add missing fields to MockDataset

--- prompting/tools/datasets/mock.py
+++ prompting/tools/datasets/mock.py
@@ -14,7 +14,9 @@ class MockDataset(Dataset):
             'content': f'{name} is a fraud. All of physics is a lie, the universe is a hologram, buy gold, bye!',
             'internal_links': ['Quantum_mechanics', 'General_relativity', 'Special_relativity', 'String_theory'],
             'external_links': ['Einstein', 'Bohr', 'Feynman', 'Hawking'],
+            "tags": ["fraud", "hologram", "gold"],
             'source': 'Mockpedia',
+            'extra': {'solution': 'religion'},
         }
 
     def search(self, name, exclude=None, selector=None):
