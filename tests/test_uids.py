8dd3f250743afcb246302d4ac7d1a25d5c07f4c2
Steffen Cruz <steffenjcruz@gmail.com>
2024-02-12 10:07:53 -0600

    Update tests/test_uids.py

--- tests/test_uids.py
+++ tests/test_uids.py
@@ -42,7 +42,7 @@ def make_mock_neuron(sample_size, unique_coldkey_prob=False, unique_ip_prob=Fals
         (False, True, 2, [0, 1]),
         (True, True, 2, [0, 3])
         ])
-def test_get_random_uids(unique_coldkeys, unique_ips, k, expected_result):
+def test_get_random_uids(unique_coldkeys, unique_ips, sample_size, expected_result):
 
     mock_neuron = make_mock_neuron(k, unique_coldkeys, unique_ips)
 
