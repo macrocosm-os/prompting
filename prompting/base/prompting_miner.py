32e2fbf548ddf37f9dd8a1503ea9015ed847cc3e
mccrindlebrian <mccrinbc@gmail.com>
2024-02-13 11:52:01 -0800

    dsstore gitignore, change miner base

--- prompting/base/prompting_miner.py
+++ prompting/base/prompting_miner.py
@@ -25,13 +25,17 @@ from prompting.protocol import PromptingSynapse
 from prompting.base.miner import BaseMinerNeuron
 from datetime import datetime
 
-class BasePromptingMiner(BaseMinerNeuron):
+class BaseStreamPromptingMiner(BaseMinerNeuron):
     """
-    Your miner neuron class. You should use this class to define your miner's behavior. In particular, you should replace the forward function with your own logic. You may also want to override the blacklist and priority functions according to your needs.
+    Your miner neuron class. You should use this class to define your miner's behavior. 
+    In particular, you should replace the forward function with your own logic. You may also want to override the blacklist and priority functions according to your needs.
 
-    This class inherits from the BaseMinerNeuron class, which in turn inherits from BaseNeuron. The BaseNeuron class takes care of routine tasks such as setting up wallet, subtensor, metagraph, logging directory, parsing config, etc. You can override any of the methods in BaseNeuron if you need to customize the behavior.
+    This class inherits from the BaseMinerNeuron class, which in turn inherits from BaseNeuron. 
+    The BaseNeuron class takes care of routine tasks such as setting up wallet, subtensor, metagraph, logging directory, parsing config, etc. 
+    You can override any of the methods in BaseNeuron if you need to customize the behavior.
 
-    This class provides reasonable default behavior for a miner such as blacklisting unrecognized hotkeys, prioritizing requests based on stake, and forwarding requests to the forward function. If you need to define custom
+    This class provides reasonable default behavior for a miner such as blacklisting unrecognized hotkeys, prioritizing requests based on stake, and forwarding requests to the forward function. 
+    If you need to define custom
     """
 
     def __init__(self, config=None):
