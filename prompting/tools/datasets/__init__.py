b2c842501de8cc9331a780da50898f631d6fcad2
Steffen Cruz <steffenjcruz@gmail.com>
2024-02-19 09:58:48 -0600

    Add init

---
+++ prompting/tools/datasets/__init__.py
@@ -0,0 +1,6 @@
+from .context import Context
+from .base import Dataset
+from .code import HFCodingDataset, StackOverflowDataset
+from .math import MathDataset
+from .mock import MockDataset
+from .wiki import WikiDataset, WikiDateDataset
