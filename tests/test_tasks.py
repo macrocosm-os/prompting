77ee3ed3f8823f5eca8ca0309d406a3431df8db9
Steffen Cruz <steffenjcruz@gmail.com>
2024-02-19 13:35:06 -0600

    Add tests for reward and penalty definitions and make test_task_field_is_not_null more explicit

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,6 +1,8 @@
 import pytest
-
+import inspect
+from inspect import signature
 from prompting.tasks import Task
+from prompting.rewards import REWARD_MODELS
 from .fixtures.task import CONTEXTS, TASKS, TASK_FIELDS
 from .fixtures.llm import LLM_PIPELINE
 
@@ -34,7 +36,7 @@ def test_task_field_has_expected_type(task: Task, field: str, expected_type: typ
 @pytest.mark.parametrize('field', TASK_FIELDS.keys())
 def test_task_field_is_not_null(task: Task, field: str):
     task = task(llm_pipeline=LLM_PIPELINE, context=CONTEXTS[task])
-    assert getattr(task, field)
+    assert getattr(task, field) is not None
 
 
 @pytest.mark.parametrize('task', TASKS)
@@ -70,3 +72,42 @@ def test_task_state_dict(task: Task, full: bool):
 
     task = task(llm_pipeline=LLM_PIPELINE, context=CONTEXTS[task])
     assert type(task.__state_dict__(full)) == dict
+
+
+@pytest.mark.parametrize('task', TASKS)
+@pytest.mark.parametrize('definition', ('reward_definition', 'penalty_definition'))
+def test_task_contains_required_definition(task: Task, definition: str):
+
+    task = task(llm_pipeline=LLM_PIPELINE, context=CONTEXTS[task])
+    model_infos = getattr(task, definition)
+    total_weight = 0
+    for model_info in model_infos:
+
+        assert isinstance(model_info, dict)
+
+        name = model_info.get("name")
+        assert name is not None
+        assert name in REWARD_MODELS
+
+        params = {k: v for k, v in model_info.items() if k not in ["name", "weight"]}
+        cls_params = signature(REWARD_MODELS['rouge']).parameters
+        # check that all the parameters are in the class (no extra parameters are allowed)
+        for k, v in params.items():
+            assert k in cls_params
+            # check that the type of the parameter is correct or not annotated
+            assert cls_params[k].annotation == inspect._empty or isinstance(v, cls_params[k].annotation)
+
+        # check that all class parameters without default values are in the model_info
+        for k, v in cls_params.items():
+            # ignore self, device, args, kwargs
+            if v.default == inspect._empty and v.name not in ("self", "device", "args", "kwargs"):
+                assert k in params
+
+        weight = model_info.get("weight")
+        assert weight is not None
+        assert isinstance(weight, (float, int))
+        assert 0 <= weight <= 1
+
+        total_weight += weight
+
+    assert not model_infos or total_weight == 1
