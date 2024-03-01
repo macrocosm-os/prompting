7719a480c2568a9ff6e59ee641b65f8fbb60f290
p-ferreira <38992619+p-ferreira@users.noreply.github.com>
2024-03-01 15:29:43 -0500

    drops unnecessary run code for base miner

--- prompting/base/prompting_miner.py
+++ prompting/base/prompting_miner.py
@@ -189,15 +189,3 @@ class BasePromptingMiner(BaseMinerNeuron):
         bt.logging.info(
             f"Miner running:: network: {self.subtensor.network} | step: {self.step} | uid: {self.uid} | trust: {m.trust[self.uid]:.3f} | emission {m.emission[self.uid]:.3f}"
         )
-
-
-# This is the main function, which runs the miner.
-if __name__ == "__main__":
-    with BasePromptingMiner() as miner:
-        while True:
-            miner.log_status()
-            time.sleep(5)
-
-            if miner.should_exit:
-                bt.logging.warning("Ending miner...")
-                break
