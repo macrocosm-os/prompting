6d34528dc4012a5cf7d9db57f3d6b69e2a7e7a96
p-ferreira <38992619+p-ferreira@users.noreply.github.com>
2024-03-01 13:43:57 -0500

    update docstring

--- prompting/miners/openai_miner.py
+++ prompting/miners/openai_miner.py
@@ -103,9 +103,6 @@ class OpenAIMiner(BasePromptingMiner):
 
         Returns:
             PromptingSynapse: The synapse object with the 'completion' field set to the miner output
-
-        The 'forward' function is a placeholder and should be overridden with logic that is appropriate for
-        the miner's intended operation. This method demonstrates a basic transformation of input data.
         """
         try:
             with get_openai_callback() as cb:
