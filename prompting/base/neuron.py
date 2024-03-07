d44228c1efac703da7de489e5ddc7dfd47e4dc37
mccrindlebrian <mccrinbc@gmail.com>
2024-03-06 18:52:36 -0800

    black

--- prompting/base/neuron.py
+++ prompting/base/neuron.py
@@ -102,12 +102,10 @@ class BaseNeuron(ABC):
         self.step = 0
 
     @abstractmethod
-    def forward(self, synapse: bt.Synapse) -> bt.Synapse:
-        ...
+    def forward(self, synapse: bt.Synapse) -> bt.Synapse: ...
 
     @abstractmethod
-    def run(self):
-        ...
+    def run(self): ...
 
     def sync(self):
         """
