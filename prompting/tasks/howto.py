b138cda2d2f8d1c1f9c62a7a7571ba957f11b5ce
Steffen Cruz <steffenjcruz@gmail.com>
2024-03-26 13:57:48 +0000

    Add WIP howto task

---
+++ prompting/tasks/howto.py
@@ -0,0 +1,26 @@
+from prompting.tasks import Task
+
+
+class HowToTask(Task):
+    name = "how to"
+    desc = "get help translating text"
+    goal = "to translate the following text"
+
+    reward_definition = [
+        dict(name="rouge", weight=1.0),
+    ]
+    penalty_definition = []
+
+    static_reference = True
+    static_query = True
+
+    def __init__(self, llm_pipeline, context, create_reference=True):
+        self.context = context
+
+        # TODO:
+        self.query = f"How do I {context.title}?"
+        self.reference = context.content
+
+        self.topic = context.title
+        self.subtopic = context.topic
+        self.tags = context.tags
