402ac2233e9fb102769e6fad4692858ba20c83f7
p-ferreira <38992619+p-ferreira@users.noreply.github.com>
2024-03-27 10:50:55 -0400

    updates versioning

--- prompting/__init__.py
+++ prompting/__init__.py
@@ -16,7 +16,7 @@
 # DEALINGS IN THE SOFTWARE.
 
 # Define the version of the template module.
-__version__ = "1.2.1"
+__version__ = "1.3.0"
 version_split = __version__.split(".")
 __spec_version__ = (
     (10000 * int(version_split[0]))
