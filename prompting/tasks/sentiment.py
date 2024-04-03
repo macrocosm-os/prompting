4225201a150083edc78ecdcd26741897e7343491
Steffen Cruz <steffenjcruz@gmail.com>
2024-04-03 14:34:29 -0600

    Refactor prompt templates and remove unused dataclass decorator

--- prompting/tasks/sentiment.py
+++ prompting/tasks/sentiment.py
@@ -1,24 +1,30 @@
 from prompting.tasks import Task
-from prompting.cleaners.cleaner import CleanerPipeline
+
+QUERY_PROMPT_TEMPLATE = """\
+
+You are a review-generating expert, focusing on making highly reaslistic revies. Your response contains only the review, nothing more, nothing less. You will adhere to a word limit of 250 words. Ask a specific question about the following context:
+
+{context}
+"""
 
 
 class SentimentAnalysisTask(Task):
     name = "sentiment analysis"
     desc = "get help analyzing the sentiment of a review"
     goal = "to get the sentiment to the following review"
+
     reward_definition = [
-        dict(name="sentiment", weight=1.0),
+        dict(name="ordinal", weight=1.0),
     ]
     penalty_definition = []
     cleaning_pipeline = []
 
     static_reference = True
-    query_system_prompt = """You are an assistant that generates reviews based on user prompts. You follow all of the user instructions as well as you can. Make the reviews as realistic as possible. Your response contains only the review, nothing more, nothing less"""
 
     def __init__(self, llm_pipeline, context, create_reference=True):
         self.context = context
 
-        self.query_prompt = context.content
+        self.query_prompt = QUERY_PROMPT_TEMPLATE.format(context=context.content)
         self.query = self.generate_query(llm_pipeline)
         self.reference = context.subtopic
 
