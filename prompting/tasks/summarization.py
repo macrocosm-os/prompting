0558153834512a9f111480f3f1c7c7260ab3edac
Steffen Cruz <steffenjcruz@gmail.com>
2024-04-03 14:32:48 -0600

    Refactor prompt templates and remove unused dataclass decorator

--- prompting/tasks/summarization.py
+++ prompting/tasks/summarization.py
@@ -1,32 +1,20 @@
-from dataclasses import dataclass
 from prompting.tasks import Task
 from transformers import Pipeline
 
 
-# TODO: introduce criteria for the query and reference answer (length, layout, etc.) and make these arguments
-
-# TODO: Also add a query system prompt and a query prompt template
-# TODO: Add the option to generate the summary query from the context. e.g. "the childhood of Abraham Lincoln" which is more specific than summarizing the entire article (Abraham Lincoln)
-
-# Used to instruct the LLM to provide a good answer to the query when given a context
-SUMMARIZATION_SYSTEM_PROMPT = """\
-You are a summarization AI assistant. You make excellent and concise summaries that adhere to your given instructions.
-You will maintain a neutral tone in your summaries.
-You will adhere to a word limit of 250 words for each response.
-"""
-
 # Used to obtain reference answer
 REFERENCE_PROMPT_TEMPLATE = """\
+
+You are a summarization expert. You make excellent and concise summaries that adhere to your given instructions. Where applicable, you will include references to credible sources to support your answers. You will adhere to a word limit of 250 words.
+
 Summarize the following context in a concise and accurate manner:
 
-## Context
+# Context:
 {context}
 """
 
 
-@dataclass
 class SummarizationTask(Task):
-
     name = "summarization"
     desc = "get help with summarization"
     goal = "summarize the following topic"
@@ -48,13 +36,11 @@ class SummarizationTask(Task):
     static_query = True
 
     def __init__(self, llm_pipeline: Pipeline, context: str, create_reference=True):
-
         self.context = context
 
         # Query is just the article title and section name
         self.query = context.title + ", " + context.topic
 
-        self.reference_system_prompt = SUMMARIZATION_SYSTEM_PROMPT
         self.reference_prompt = REFERENCE_PROMPT_TEMPLATE.format(
             context=context.content
         )
