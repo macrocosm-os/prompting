d043557ee283178c90163195200b8a39e7931af8
mccrindlebrian <mccrinbc@gmail.com>
2024-04-03 13:13:41 -0500

    change dendrite timings to be more robust, and change status code

--- prompting/dendrite.py
+++ prompting/dendrite.py
@@ -8,14 +8,29 @@ class DendriteResponseEvent:
         self, responses: List[bt.Synapse], uids: torch.LongTensor, timeout: float
     ):
         self.uids = uids
-        self.completions = [synapse.completion for synapse in responses]
-        self.timings = [
-            synapse.dendrite.process_time or timeout for synapse in responses
-        ]
-        self.status_messages = [
-            synapse.dendrite.status_message for synapse in responses
-        ]
-        self.status_codes = [synapse.dendrite.status_code for synapse in responses]
+        self.completions = []
+        self.status_messages = []
+        self.status_codes = []
+        self.timings = []
+
+        for synapse in responses:
+            self.completions.append(synapse.completion)
+            self.status_messages.append(synapse.dendrite.status_message)
+
+            if len(synapse.completion) == 0 and synapse.dendrite.status_code == 200:
+                synapse.dendrite.status_code = 204
+
+            self.status_codes.append(synapse.dendrite.status_code)
+
+            if (synapse.dendrite.process_time) and (
+                synapse.dendrite.status_code == 200
+                or synapse.dendrite.status_code == 204
+            ):
+                self.timings.append(synapse.dendrite.process_time)
+            elif synapse.dendrite.status_code == 408:
+                self.timings.append(timeout)
+            else:
+                self.timings.append(0)  # situation where miner is not alive
 
     def __state_dict__(self):
         return {
