473805f5cfd5d969ebb68798fe319f544135c92b
bkb2135 <bkb2135@columbia.edu>
2024-04-04 16:24:58 +0000

    Catch empty completions for sentiment

--- prompting/rewards/ordinal.py
+++ prompting/rewards/ordinal.py
@@ -4,23 +4,37 @@ from typing import List
 from prompting.rewards import BaseRewardModel, BatchRewardOutput
 
 
-class CategoricalDistance(BaseRewardModel):
+class OrdinalRewardModel(BaseRewardModel):
     @property
     def name(self) -> str:
         return "category_distance"
 
     def __init__(self, **kwargs):
         super().__init__()
+        #TODO: Expand to allow for more than 3 classes (Must also adjust dataset/review.py)
+        self.sentiments = [
+            "casual",
+            "basic",
+            "silly",
+            "random",
+            "thoughtful",
+            "serious",
+            "rushed",
+        ]
+        #NOTE: These sentimens are not the same as the sentiments defined in the dataset/review.py file. These are the subtopic
+
 
     def reward(self, reference: str, completions: List[str]) -> BatchRewardOutput:
         """Compute difference scores given a completion and reference pair."""
         rewards = []
         timings = []
         classes = self.sentiments
-
         for completion in completions:
             t0 = time.time()
-            reward = abs(classes.index(reference) - classes.index(completion))
+            if completion not in classes:
+                reward = 0
+            else:
+                reward = abs(classes.index(reference) - classes.index(completion))
             timings.append(time.time() - t0)
             rewards.append(reward)
 
