df1885dbd6859864bb095d25f2c2114b1eae9370
bkb2135 <bkb2135@columbia.edu>
2024-04-08 15:26:14 +0000

    Create separate task_registry file

---
+++ prompting/task_registry.py
@@ -0,0 +1,8 @@
+# TODO: Expand this to include extra information beyond just the task and dataset names
+TASK_REGISTRY = {
+    "summarization": ["wiki"],
+    "qa": ["wiki"],
+    "debugging": ["hf_coding"],
+    "math": ["math"],
+    "date_qa": ["wiki_date"],
+}
