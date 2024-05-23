03e37bdba88a77fa3e02159a9eb9ec3bab9de1e7
Dmytro Bobrenko <17252809+dbobrenko@users.noreply.github.com>
2024-05-23 12:40:34 +0000

    Update miner deps

--- install.sh
+++ install.sh
@@ -5,3 +5,8 @@ pip uninstall mathgenerator -y
 
 # Installing package from the current directory
 pip install -e .
+
+# Install AutoAWQ without dependencies, to avoid conflicts with lower version of transformers.
+pip install zstandard --no-deps
+pip installautoawq-kernels --no-deps
+pip install autoawq==0.2.5 --no-deps
