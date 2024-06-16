56de3fc34a13095816e94869e8303ddb85a940ad
Chengzhe <chengzhe8919@gmail.com>
2024-06-16 14:31:52 +0000

    feat: Add PM2 installation commands to install.sh for streamlined setup
    - Update package list and install jq and npm
    - Install PM2 globally and update PM2

--- install.sh
+++ install.sh
@@ -5,3 +5,12 @@ pip uninstall mathgenerator -y
 
 # Installing package from the current directory
 pip install -e .
+
+# Updating the package list and installing jq and npm
+sudo apt update && sudo apt install -y jq npm
+
+# Installing PM2 globally
+sudo npm install pm2 -g
+
+# Updating PM2
+pm2 update
