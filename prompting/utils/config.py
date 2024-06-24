39c487c70cbe0e847f2bdb95937141b015eb2188
bkb2135 <98138173+bkb2135@users.noreply.github.com>
2024-06-24 08:23:57 -0400

    Reduce timout to 15 seconds

--- prompting/utils/config.py
+++ prompting/utils/config.py
@@ -307,7 +307,7 @@ def add_validator_args(cls, parser):
         "--neuron.timeout",
         type=float,
         help="The timeout for each forward call in seconds.",
-        default=17,
+        default=15,
     )
 
     parser.add_argument(
