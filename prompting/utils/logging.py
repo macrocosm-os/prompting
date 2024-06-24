3556e9781369465c522664f84665281ad23c04bb
bkb2135 <bkb2135@columbia.edu>
2024-06-24 17:36:59 +0000

    Change event logging

--- prompting/utils/logging.py
+++ prompting/utils/logging.py
@@ -106,7 +106,7 @@ def reinit_wandb(self):
 
 def log_event(self, event):
     if not self.config.neuron.dont_save_events:
-        logger.log(38, "events", **event)
+        logger.log(38, event)
 
     if self.config.wandb.off:
         return
