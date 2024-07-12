c4988be71e2e6afdbad4b49f58ccdd269fba2a05
bkb2135 <98138173+bkb2135@users.noreply.github.com>
2024-07-12 10:28:21 -0400

    Point to macrocosmos entity

--- prompting/utils/config.py
+++ prompting/utils/config.py
@@ -380,7 +380,7 @@ def add_validator_args(cls, parser):
         "--wandb.entity",
         type=str,
         help="The name of the project where you are sending the new run.",
-        default="opentensor-dev",
+        default="macrocosmos",
     )
 
     parser.add_argument(
