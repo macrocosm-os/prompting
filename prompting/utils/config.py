bdad04cd8f9d46489ea0aef594def8e6add87eaf
bkb2135 <bkb2135@columbia.edu>
2024-07-12 20:33:19 +0000

    Adjust project name

--- prompting/utils/config.py
+++ prompting/utils/config.py
@@ -373,7 +373,7 @@ def add_validator_args(cls, parser):
         "--wandb.project_name",
         type=str,
         help="The name of the project where you are sending the new run.",
-        default="alpha-validators",
+        default="prompting-validators",
     )
 
     parser.add_argument(
