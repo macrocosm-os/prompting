40b2cee3ea64800656a92ec3c8cbf159067bdf34
Hollyqui <felix.quinque@macrocosmos.ai>
2024-07-16 13:01:10 +0000

    SN1-110: Attempting to fix CI/CD

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,2 +1,1 @@
-python neurons/validator.py --netuid 61 --subtensor.network test --neuron.device cuda --wallet.name validator \
---wallet.hotkey validator_hotkey --logging.debug --axon.port 70001   
+python neurons/validator.py --netuid 61 --subtensor.network test --neuron.device cuda --wallet.name validator --wallet.hotkey validator_hotkey --logging.debug --axon.port 22044 --neuron.model_id casperhansen/llama-3-8b-instruct-awq --neuron.llm_max_allowed_memory_in_gb 24 --neuron.gpus 1 --wandb.off
