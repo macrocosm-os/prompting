00d67a40732b831ac26c3bfc644b5ace7655f22b
bkb2135 <98138173+bkb2135@users.noreply.github.com>
2024-07-15 22:48:29 -1000

    v2.5.2 (#287)
    * Point to macrocosmos entity
    
    * Adjust project name
    
    * Increase spec version to 2.5.2

--- prompting/utils/config.py
+++ prompting/utils/config.py
@@ -373,14 +373,14 @@ def add_validator_args(cls, parser):
         "--wandb.project_name",
         type=str,
         help="The name of the project where you are sending the new run.",
-        default="alpha-validators",
+        default="prompting-validators",
     )
 
     parser.add_argument(
         "--wandb.entity",
         type=str,
         help="The name of the project where you are sending the new run.",
-        default="opentensor-dev",
+        default="macrocosmos",
     )
 
     parser.add_argument(
