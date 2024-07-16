4076ee497adff181c00fb3c3578d104466c4501f
Felix Quinque <40171911+Hollyqui@users.noreply.github.com>
2024-07-16 11:05:37 +0100

    Removing unnecessary newline
    Co-authored-by: Dmytro Bobrenko <17252809+dbobrenko@users.noreply.github.com>

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,2 +1,2 @@
-python neurons/validator.py --netuid 61 --subtensor.network test --neuron.device cuda --wallet.name validator --wallet.h
-otkey validator_hotkey --logging.debug --axon.port 70001   
+python neurons/validator.py --netuid 61 --subtensor.network test --neuron.device cuda --wallet.name validator \
+--wallet.hotkey validator_hotkey --logging.debug --axon.port 70001   
