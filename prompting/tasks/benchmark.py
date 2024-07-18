51ac8169088e8748f939433f76898fba73c18467
steffencruz <steffenjcruz@gmail.com>
2024-07-18 13:34:29 +0100

    Add missing imports and black

--- prompting/tasks/benchmark.py
+++ prompting/tasks/benchmark.py
@@ -1,6 +1,7 @@
 import re
 import numpy as np
 import bittensor as bt
+from typing import List, Tuple
 from dataclasses import dataclass
 from prompting.tasks import Task
 from prompting.utils.exceptions import TaskCreationError
