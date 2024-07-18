ebed8574dfd45e0a77904802557c816aef43a503
steffencruz <steffenjcruz@gmail.com>
2024-07-18 10:25:02 +0100

    Add shuffling to choices to ensure uniform distribution of reference positions

--- prompting/tasks/benchmark.py
+++ prompting/tasks/benchmark.py
@@ -1,4 +1,5 @@
 import re
+import numpy as np
 import bittensor as bt
 from dataclasses import dataclass
 from prompting.tasks import Task
@@ -63,17 +64,18 @@ class BenchmarkingTask(Task):
     static_reference = True
     challenge_type = "query"
 
-    # specific pattern (semi-flexible) which detects multiple choices 
-    choices_pattern = r"\n\s*\*\s*\W?([A-D])\W?\s*(.*)"
-    
+    # specific pattern (semi-flexible) which detects multiple choices
+    choices_pattern = r"\n\s*(\*?\s*\W?[A-D]\W?)\s*(.*)"
+
     def __init__(self, llm_pipeline, context, create_reference=True):
         self.context = context
 
         self.query_system_prompt = QUERY_SYSTEM_PROMPT
-        self.query_prompt = QUERY_PROMPT_TEMPLATE.format(context=context.content, title=context.title, source=context.source)
+        self.query_prompt = QUERY_PROMPT_TEMPLATE.format(
+            context=context.content, title=context.title, source=context.source
+        )
 
         query_with_choices = self.generate_query(llm_pipeline)
-        
 
         self.query, self.reference = self.extract_query_and_reference(
             query_with_choices
@@ -83,27 +85,65 @@ class BenchmarkingTask(Task):
         self.subtopic = context.topic
         self.tags = context.tags
 
-    def extract_query_and_reference(self, query_with_choices):
+    def extract_query_and_reference(self, query_with_choices: str) -> Tuple[str, str]:
         """
-        Extract the query and reference answer from the generated query. 
+        Extract the query and reference answer from the generated query.
         To do this we extract the reference answer by searching for the choice with a * symbol,
         and then removing the * to form the query
         """
+        # get the index of first occurrence of the choices
+        index = re.search(self.choices_pattern, query_with_choices).start()
 
-        reference_matches = re.findall(self.choices_pattern, query_with_choices)
-        if len(reference_matches) == 0:
+        items, choices = list(
+            zip(*re.findall(self.choices_pattern, query_with_choices[index:]))
+        )
+        if len(choices) != 4:
             raise TaskCreationError(
-                f"{self.__class__.__name__} failed to extract query and reference from query_with_choices:\n{query_with_choices}"
+                f"{self.__class__.__name__} the number of choices is not 4 in query_with_choices:\n{query_with_choices}"
             )
-        elif len(reference_matches) > 1:
+
+        correct_item = [i for i, item in enumerate(items) if "*" in item]
+        if len(correct_item) == 0:
+            raise TaskCreationError(
+                f"{self.__class__.__name__} no reference was found in query_with_choices:\n{query_with_choices}"
+            )
+        elif len(correct_item) != 1:
             raise TaskCreationError(
                 f"{self.__class__.__name__} found multiple reference matches in query_with_choices:\n{query_with_choices}"
             )
+        reference_label = choices[correct_item[0]]
 
-        correct_letter, _ = reference_matches[0]
-        # remove the asterisk which reveals the correct answer
-        query = "\n".join(
-            [line.strip("* ") for line in query_with_choices.splitlines()]
+        shuffled_choices, new_reference = self.shuffle_choices(choices, reference_label)
+        shuffled_query_with_choices = (
+            query_with_choices[:index] + "\n\n" + shuffled_choices
+        )
+        return shuffled_query_with_choices, new_reference
+
+    def shuffle_choices(
+        self, choices: List[str], reference_label: str
+    ) -> Tuple[str, str]:
+        """Shuffle the choices and return the new reference. By itself, the LLM will almost always assign the reference to option B or C.
+        This method overcomes the highly biased ordering by manually reshuffling the choices so that the reference is uniformly distributed across the options.
+
+        Args:
+            choices (list): list of choices
+            reference_label (str): the reference answer label
+
+        Returns:
+            str: the shuffled choices
+            str: the new reference, in {A, B, C, D}
+        """
+        # shuffle the choices
+        shuffled_choice_list = list(
+            zip("ABCD", np.random.choice(choices, len(choices), replace=False))
         )
 
-        return query, correct_letter
+        # match the reference and get the letter
+        new_reference = [c[0] for c in shuffled_choice_list if c[1] == reference_label][
+            0
+        ]
+
+        # reconstruct the shuffled choices (without indicating which is correct)
+        shuffled_choices = "\n".join([f"{c[0]}. {c[1]}" for c in shuffled_choice_list])
+
+        return shuffled_choices, new_reference
