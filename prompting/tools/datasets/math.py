7de2fc176b3f0bca7b2065b5d2e03c76b974d34c
bkb2135 <bkb2135@columbia.edu>
2024-07-24 20:49:23 +0000

    Adjust Math Dataset to allow non-float solutions.

--- prompting/tools/datasets/math.py
+++ prompting/tools/datasets/math.py
@@ -22,29 +22,28 @@ import itertools
 import mathgenerator
 import bittensor as bt
 from sympy.parsing.latex import parse_latex
-from typing import Dict, Union, List, Tuple
-
+from typing import Dict, Union, List, Tuple, Optional
 
 from .base import Dataset
 from ..selector import Selector
 
 
 class MathDataset(Dataset):
-    name = 'math'
-    topics_list = mathgenerator.getGenList()
+    name: str = "math"
+    topics_list: List = mathgenerator.getGenList()
 
-    def __init__(self, seed=None):
+    def __init__(self, seed: Optional[int] = None) -> None:
         self.seed = seed
         self.rng = random.Random(seed)
 
     def get(
         self,
         name: str,
-        selector: Selector = None,
-        include: List = None,
-        exclude: List = None,
+        selector: Optional[Selector] = None,
+        include: Optional[List[str]] = None,
+        exclude: Optional[List[str]] = None,
         **kwargs,
-    ) -> Dict:
+    ) -> Dict[str, Union[str, List[str], Dict[str, str]]]:
         """Get a math problem.
 
         Args:
@@ -60,42 +59,51 @@ class MathDataset(Dataset):
         max_tries = 10
         for _ in range(max_tries):
             info = mathgenerator.generate_context(name, **kwargs)
-            if info["reward_type"] != "float" or info["topic"] == "computer_science":       
-                pass
-            else:
-                math_words = [
-                    "math",
-                    "mathematics",
-                    "mathematical",
-                    "math problem",
-                    "math technique",
-                ]
-                external_links = []
-                # construct external links from randomly shuffled trigrams containing 2 words from the problem and 1 random math word
-                # binary_to_decimal -> ['binary to', 'to decimal']
-                for bigram in itertools.combinations(info["forward_words"], 2):
-                    words = list(bigram) + [random.choice(math_words)]
-                    # shuffle the words e.g. ['binary', 'decimal', 'math problem'] -> 'decimal binary math problem'
-                    external_links.append(" ".join(random.sample(words, len(words))))
+            # Extra check to ensure that binary_to_decimal is not casted as a float.
+            if info["topic"] == "computer_science":
+                info["reward_type"] = "symbolic"
+            math_words = [
+                "math",
+                "mathematics",
+                "mathematical",
+                "math problem",
+                "math technique",
+            ]
+            external_links = []
+            # construct external links from randomly shuffled trigrams containing 2 words from the problem and 1 random math word
+            # binary_to_decimal -> ['binary to', 'to decimal']
+            for bigram in itertools.combinations(info["forward_words"], 2):
+                words = list(bigram) + [random.choice(math_words)]
+                # shuffle the words e.g. ['binary', 'decimal', 'math problem'] -> 'decimal binary math problem'
+                external_links.append(" ".join(random.sample(words, len(words))))
 
-                return {
-                    "title": info["topic"],  # title of math problem
-                    "topic": info["topic"],  # title of problem topic
-                    "subtopic": info["subtopic"],  # title of problem subtopic
-                    "content": info["problem"],  # problem statement
-                    "internal_links": [info["topic"], info["subtopic"]],  # internal links
-                    "external_links": external_links,
-                    "tags": info["forward_words"],
-                    "extra": {"reward_type": info["reward_type"], "solution": info["solution"]},
-                }
+            return {
+                "title": info["topic"],  # title of math problem
+                "topic": info["topic"],  # title of problem topic
+                "subtopic": info["subtopic"],  # title of problem subtopic
+                "content": info["problem"],  # problem statement
+                "internal_links": [info["topic"], info["subtopic"]],  # internal links
+                "external_links": external_links,
+                "tags": info["forward_words"],
+                "extra": {
+                    "reward_type": info["reward_type"],
+                    "solution": info["solution"],
+                },
+            }
 
     def search(
-        self, name, selector: Selector, include: List = None, exclude: List = None
+        self,
+        name: str,
+        selector: Selector,
+        include: Optional[List[str]] = None,
+        exclude: Optional[List[str]] = None,
     ) -> Dict:
         raise NotImplementedError(
             f"Search is not implemented for {self.__class__.__name__}"
         )
 
-    def random(self, selector: Selector, **kwargs):
+    def random(
+        self, selector: Selector, **kwargs
+    ) -> Dict[str, Union[str, List[str], Dict[str, str]]]:
         """Create a random math problem."""
         return self.get(name=None, selector=selector, **kwargs)
