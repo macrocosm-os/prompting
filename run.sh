e61c9ad06c21b8104c7419304e2ad3b366593de5
bkb2135 <bkb2135@columbia.edu>
2024-07-31 13:49:36 +0000

    Clean up run and check update scripts

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@
 script="neurons/validator.py"
 autoRunLoc=$(readlink -f "$0")
 proc_name="s1_validator_main_process"
+update_proc_name="check_updates"
 args=()
 version_location="./prompting/__init__.py"
 version="__version__"
@@ -22,135 +23,6 @@ poetry install --extras "validator"
 
 # Uninstall uvloop
 poetry run pip uninstall -y uvloop
-
-# Checks if $1 is smaller than $2
-# If $1 is smaller than or equal to $2, then true.
-# else false.
-version_less_than_or_equal() {
-    [  "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
-}
-
-# Checks if $1 is smaller than $2
-# If $1 is smaller than $2, then true.
-# else false.
-version_less_than() {
-    [ "$1" = "$2" ] && return 1 || version_less_than_or_equal $1 $2
-}
-
-# Returns the difference between
-# two versions as a numerical value.
-get_version_difference() {
-    local tag1="$1"
-    local tag2="$2"
-
-    # Extract the version numbers from the tags
-    local version1=$(echo "$tag1" | sed 's/v//')
-    local version2=$(echo "$tag2" | sed 's/v//')
-
-    # Split the version numbers into an array
-    IFS='.' read -ra version1_arr <<< "$version1"
-    IFS='.' read -ra version2_arr <<< "$version2"
-
-    # Calculate the numerical difference
-    local diff=0
-    for i in "${!version1_arr[@]}"; do
-        local num1=${version1_arr[$i]}
-        local num2=${version2_arr[$i]}
-
-        # Compare the numbers and update the difference
-        if (( num1 > num2 )); then
-            diff=$((diff + num1 - num2))
-        elif (( num1 < num2 )); then
-            diff=$((diff + num2 - num1))
-        fi
-    done
-
-    strip_quotes $diff
-}
-
-read_version_value() {
-    # Read each line in the file
-    while IFS= read -r line; do
-        # Check if the line contains the variable name
-        if [[ "$line" == *"$version"* ]]; then
-            # Extract the value of the variable
-            local value=$(echo "$line" | awk -F '=' '{print $2}' | tr -d ' ')
-            strip_quotes $value
-            return 0
-        fi
-    done < "$version_location"
-
-    echo ""
-}
-
-check_package_installed() {
-    local package_name="$1"
-    os_name=$(uname -s)
-
-    if [[ "$os_name" == "Linux" ]]; then
-        # Use dpkg-query to check if the package is installed
-        if dpkg-query -W -f='${Status}' "$package_name" 2>/dev/null | grep -q "installed"; then
-            return 1
-        else
-            return 0
-        fi
-    elif [[ "$os_name" == "Darwin" ]]; then
-         if brew list --formula | grep -q "^$package_name$"; then
-            return 1
-        else
-            return 0
-        fi
-    else
-        echo "Unknown operating system"
-        return 0
-    fi
-}
-
-check_variable_value_on_github() {
-    local repo="$1"
-    local file_path="$2"
-    local variable_name="$3"
-
-    local url="https://api.github.com/repos/$repo/contents/$file_path"
-    local response=$(curl -s "$url")
-
-    # Check if the response contains an error message
-    if [[ $response =~ "message" ]]; then
-        echo "Error: Failed to retrieve file contents from GitHub."
-        return 1
-    fi
-
-    # Extract the content from the response
-    local content=$(echo "$response" | tr -d '\n' | jq -r '.content')
-
-    if [[ "$content" == "null" ]]; then
-        echo "File '$file_path' not found in the repository."
-        return 1
-    fi
-
-    # Decode the Base64-encoded content
-    local decoded_content=$(echo "$content" | base64 --decode)
-
-    # Extract the variable value from the content
-    local variable_value=$(echo "$decoded_content" | grep "$variable_name" | awk -F '=' '{print $2}' | tr -d ' ')
-
-    if [[ -z "$variable_value" ]]; then
-        echo "Variable '$variable_name' not found in the file '$file_path'."
-        return 1
-    fi
-
-    strip_quotes $variable_value
-}
-
-strip_quotes() {
-    local input="$1"
-
-    # Remove leading and trailing quotes using parameter expansion
-    local stripped="${input#\"}"
-    stripped="${stripped%\"}"
-
-    echo "$stripped"
-}
 
 # Loop through all command line arguments
 while [[ $# -gt 0 ]]; do
@@ -181,23 +53,16 @@ while [[ $# -gt 0 ]]; do
   fi
 done
 
-# Check if script argument was provided
-if [[ -z "$script" ]]; then
-    echo "The --script argument is required."
-    exit 1
-fi
-
-branch=$(git branch --show-current)            # get current branch.
-echo watching branch: $branch
-echo pm2 process name: $proc_name
-
-# Get the current version locally.
-current_version=$(read_version_value)
-
 # Check if script is already running with pm2
 if pm2 status | grep -q $proc_name; then
-    echo "The script is already running with pm2. Stopping and restarting..."
+    echo "The main is already running with pm2. Stopping and restarting..."
     pm2 delete $proc_name
+fi
+
+# Check if the update check is already running with pm2
+if pm2 status | grep -q $update_proc_name; then
+    echo "The update check is already running with pm2. Stopping and restarting..."
+    pm2 delete $update_proc_name
 fi
 
 # Run the Python script with the arguments using pm2
@@ -219,7 +84,7 @@ echo "module.exports = {
       interpreter: 'python3',
       min_uptime: '5m',
       max_restarts: '5',
-      args: ['run', 'python', 'neurons/validator.py', $joined_args]
+      args: ['run', 'python', '$script', $joined_args]
     },
     {
       name: 'check_updates',
