c7241d1f63785ff8e6c7c8a23c1eccc88803fcd2
Hollyqui <felix.quinque@macrocosmos.ai>
2024-08-21 11:23:12 +0000

    SN1-195: Fixing model loading/unloading

--- prompting/llms/model_manager.py
+++ prompting/llms/model_manager.py
@@ -1,11 +1,9 @@
 from loguru import logger
 from pydantic import BaseModel, ConfigDict
 import torch
-import asyncio
 import vllm
 from prompting.llms.utils import GPUInfo
 from vllm.distributed.parallel_state import destroy_model_parallel
-from prompting.settings import settings
 from prompting.llms.model_zoo import ModelConfig, ModelZoo
 from prompting.base.loop_runner import AsyncLoopRunner
 
@@ -38,13 +36,19 @@ class ModelManager(BaseModel):
 
         # if force loading is enabled, unload models until there is enough RAM
         if force:
-            for active_model in self.active_models.keys():
+            logger.debug(f"Forcing model {model_config.model_id} to load.")
+            for active_model in list(self.active_models.keys()):
+                logger.debug(f"Checking if model {active_model.model_id} can be unloaded.")
                 if active_model in self.always_active_models:
+                    logger.debug(f"Model {active_model.model_id} is always active. Skipping.")
                     continue
                 if self.used_ram + model_config.min_ram > self.total_ram or GPUInfo.free_memory < model_config.min_ram:
                     logger.debug(f"Unloading {active_model.model_id} to make room for {model_config.model_id}")
                     self.unload_model(active_model)
                 else:
+                    logger.debug(
+                        f"Enough RAM available to load {model_config.model_id}. Free ram: {GPUInfo.free_memory}, use ram: {self.used_ram}, total ram: {self.total_ram}"
+                    )
                     break
 
         if self.used_ram + model_config.min_ram > self.total_ram or GPUInfo.free_memory < model_config.min_ram:
@@ -104,6 +108,11 @@ class AsyncModelScheduler(AsyncLoopRunner):
 
     async def run_step(self):
         """This method is called periodically according to the interval."""
+        # After the interval, unload the model if it is not in always_active_models
+        # if selected_model not in self.model_manager.always_active_models:
+        #     logger.info(f"Unloading model {selected_model.model_id} after {self.interval} seconds.")
+        #     self.model_manager.unload_model(selected_model)
+
         selected_model = ModelZoo.get_random(max_ram=self.model_manager.total_ram)
         logger.info(f"Loading model {selected_model.model_id} for {self.interval} seconds.")
 
@@ -112,19 +121,18 @@ class AsyncModelScheduler(AsyncLoopRunner):
             return
 
         # Load the selected model
-        await self.load_model_async(selected_model)
+        model_manager.load_model(selected_model)
+        # await self.load_model_async(
+        #     selected_model,
+        # )
 
-        # After the interval, unload the model if it is not in always_active_models
-        if selected_model not in self.model_manager.always_active_models:
-            logger.info(f"Unloading model {selected_model.model_id} after {self.interval} seconds.")
-            self.model_manager.unload_model(selected_model)
-
-    async def load_model_async(self, model_config: ModelConfig):
-        loop = asyncio.get_event_loop()
-        await loop.run_in_executor(None, self.model_manager.load_model, model_config, False)
+    # async def load_model_async(self, model_config: ModelConfig):
+    #     loop = asyncio.get_event_loop()
+    #     await loop.run_in_executor(None, self.model_manager.load_model, model_config, True)
 
 
 # keep model used for validation always active
 # model_manager: ModelManager
-model_manager = ModelManager(always_active_models=[ModelZoo.get_model_by_id(settings.NEURON_MODEL_ID_VALIDATOR)])
+# model_manager = ModelManager(always_active_models=[ModelZoo.get_model_by_id(settings.NEURON_MODEL_ID_VALIDATOR)])
+model_manager = ModelManager()
 model_scheduler = AsyncModelScheduler(model_manager=model_manager)
--- prompting/rewards/scoring.py
+++ prompting/rewards/scoring.py
@@ -1,15 +1,15 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import ConfigDict
 from loguru import logger
 import threading
 from prompting.tasks.base_task import BaseTextTask
 from prompting.tasks.task_registry import TaskRegistry
-import time
 from prompting.base.dendrite import DendriteResponseEvent
 from prompting.tasks.inference import model_manager
 from prompting.utils.logging import RewardLoggingEvent, log_event
 import numpy as np
 from prompting.datasets.base import DatasetEntry
 from dataclasses import dataclass
+from prompting.base.loop_runner import AsyncLoopRunner
 
 
 @dataclass
@@ -19,7 +19,7 @@ class ScoringConfig:
     dataset_entry: DatasetEntry
 
 
-class ScoringManager(BaseModel):
+class ScoringManager(AsyncLoopRunner):
     """The scoring manager maintains a queue of tasks & responses to score and then runs a scoring loop in a background thread.
     This scoring loop will score the responses once the LLM needed is loaded in the model_manager and log the rewards.
     """
@@ -34,59 +34,59 @@ class ScoringManager(BaseModel):
         logger.debug(f"SCORING: Added to queue: {task.task_id}")
         self.scoring_queue.append(ScoringConfig(task=task, response=response, dataset_entry=dataset_entry))
 
-    def scoring_step(self) -> RewardLoggingEvent:
+    def run_step(self) -> RewardLoggingEvent:
         # Only score responses for which the model is loaded
         scorable = [
             scoring_config
             for scoring_config in self.scoring_queue
             if scoring_config.task.model in model_manager.active_models.keys()
         ]
-        if len(scorable) > 0:
-            self.scoring_queue.remove(scorable[0])
-            scoring_config = scorable.pop(0)
+        if len(scorable) == 0:
+            logger.debug("Nothing to score. Skipping scoring step.")
+            return
+        self.scoring_queue.remove(scorable[0])
+        scoring_config = scorable.pop(0)
 
-            # here we generate the actual reference
-            scoring_config.task.make_reference(
-                dataset_entry=scoring_config.dataset_entry,
+        # here we generate the actual reference
+        scoring_config.task.make_reference(
+            dataset_entry=scoring_config.dataset_entry,
+        )
+
+        # and there we then calculate the reward
+        reward_pipeline = TaskRegistry.get_task_reward(scoring_config.task)
+        reward_events, penalty_events, rewards = reward_pipeline.apply(
+            response_event=scoring_config.response,
+            challenge=scoring_config.task.query,
+            reference=scoring_config.task.reference,
+        )
+        best_response = scoring_config.response.completions[np.argmax(rewards)]
+        logger.debug(f"SCORING: Scored {scoring_config.task.task_id} with reward {rewards}")
+        # Log the step event.
+        log_event(
+            RewardLoggingEvent(
+                best=best_response,
+                reward_events=reward_events,
+                penalty_events=penalty_events,
+                task_id=scoring_config.task.task_id,
             )
+        )
+        logger.debug("Scoring step completed.")
 
-            # and there we then calculate the reward
-            reward_pipeline = TaskRegistry.get_task_reward(scoring_config.task)
-            reward_events, penalty_events, rewards = reward_pipeline.apply(
-                response_event=scoring_config.response,
-                challenge=scoring_config.task.query,
-                reference=scoring_config.task.reference,
-            )
-            best_response = scoring_config.response.completions[np.argmax(rewards)]
-            logger.debug(f"SCORING: Scored {scoring_config.task.task_id} with reward {rewards}")
-            # Log the step event.
-            log_event(
-                RewardLoggingEvent(
-                    best=best_response,
-                    reward_events=reward_events,
-                    penalty_events=penalty_events,
-                    task_id=scoring_config.task.task_id,
-                )
-            )
-        else:
-            time.sleep(1)
+    # def run_scoring_loop(self) -> None:
+    #     logger.info("Starting scoring loop...")
+    #     while True:
+    #         self.scoring_step()
 
-    def run_scoring_loop(self) -> None:
-        logger.info("Starting scoring loop...")
-        while True:
-            self.scoring_step()
-            logger.debug("Scoring step completed.")
-
-    def run_in_background_thread(self):
-        """
-        Starts the validator's operations in a background thread upon entering the context.
-        This method facilitates the use of the validator in a 'with' statement.
-        """
-        if not self.is_running:
-            self.thread = threading.Thread(target=self.run_scoring_loop, daemon=True)
-            self.thread.start()
-            self.is_running = True
+    # def run_in_background_thread(self):
+    #     """
+    #     Starts the validator's operations in a background thread upon entering the context.
+    #     This method facilitates the use of the validator in a 'with' statement.
+    #     """
+    #     if not self.is_running:
+    #         self.thread = threading.Thread(target=self.run_scoring_loop, daemon=True)
+    #         self.thread.start()
+    #         self.is_running = True
 
 
 scoring_manager = ScoringManager()
-scoring_manager.run_in_background_thread()
+scoring_manager.start()
