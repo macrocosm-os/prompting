9856725debed8eecce44ba0da8aa02672f6cf53f
bkb2135 <bkb2135@columbia.edu>
2024-08-26 09:53:45 -0900

    Remove augment_query call from generate_query

--- prompting/tasks/base_task.py
+++ prompting/tasks/base_task.py
@@ -58,7 +58,7 @@ class BaseTask(ABC, BaseModel):
         """Generates a query to be used for generating the challenge"""
         logger.info("🤖 Generating query...")
         query = vLLM_LLM(llm_pipeline, system_prompt=cls.query_system_prompt or "").query(message=message)
-        return cls.augment_query(query, llm_pipeline)
+        return query
 
     @classmethod
     def augment_query(
