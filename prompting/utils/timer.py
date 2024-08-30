e5e8e9e93d7ffc3e692271a4ab64e45c3a4522bf
Felix Quinque <40171911+Hollyqui@users.noreply.github.com>
2024-08-30 11:18:59 +0100

    SN1-195: Moving to perf_counter
    Co-authored-by: Dmytro Bobrenko <17252809+dbobrenko@users.noreply.github.com>

--- prompting/utils/timer.py
+++ prompting/utils/timer.py
@@ -7,5 +7,5 @@ class Timer:
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.end_time = time.time()
+        self.end_time = time.perf_counter()
         self.elapsed_time = self.end_time - self.start_time
