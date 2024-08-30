15949d7e2b40c91e6fa22aab7ee006ac2b8246af
Felix Quinque <40171911+Hollyqui@users.noreply.github.com>
2024-08-30 11:14:38 +0100

    SN1-195: Timer to use perf_counter
    Co-authored-by: Dmytro Bobrenko <17252809+dbobrenko@users.noreply.github.com>

--- prompting/utils/timer.py
+++ prompting/utils/timer.py
@@ -3,7 +3,7 @@ import time
 
 class Timer:
     def __enter__(self):
-        self.start_time = time.time()
+        self.start_time = time.perf_counter()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
