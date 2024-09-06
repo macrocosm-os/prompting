7b5dc59ac5db987278c453f6e775d89de39d5f06
Hollyqui <felix.quinque@macrocosmos.ai>
2024-09-06 14:16:29 +0000

    SN1-212: Fixing wiki.py

--- prompting/datasets/wiki.py
+++ prompting/datasets/wiki.py
@@ -8,13 +8,13 @@ import wikipedia
 from queue import Queue, Full, Empty
 from functools import lru_cache
 from prompting.datasets.base import BaseDataset
-from prompting.datasets.base import DatasetEntry
+from prompting.datasets.base import Context
 from typing import ClassVar
 from typing import Optional
 from pydantic import model_validator, ConfigDict
 
 # Create a queue called CACHED_ARTICLES to store wikipedia articles that have been fetched
-CACHED_ARTICLES: Queue[DatasetEntry] = Queue(maxsize=300)
+CACHED_ARTICLES: Queue[Context] = Queue(maxsize=300)
 
 
 # speed up page loading
@@ -147,7 +147,7 @@ class WikiDataset(BaseDataset):
         name: str,
         exclude: list = None,
         **kwargs,
-    ) -> DatasetEntry:
+    ) -> Context:
         """Get a specified Wikipedia page and extract a section based on the selector.
 
         Args:
@@ -179,7 +179,7 @@ class WikiDataset(BaseDataset):
 
         section_length = len(selected_section[1].split())
 
-        context = DatasetEntry(
+        context = Context(
             title=name,
             topic=header or section_title,
             subtopic=section_title,
@@ -200,7 +200,7 @@ class WikiDataset(BaseDataset):
             logger.debug("Cache is full. Skipping article until cache is emptied.")
         return context
 
-    def search(self, name, results=3) -> DatasetEntry:
+    def search(self, name, results=3) -> Context:
         titles = _wikipedia_search(name, results=results)
         title = random.choice(titles)
         return self.get(title)
@@ -213,11 +213,11 @@ class WikiDataset(BaseDataset):
         return None
 
 
-class DateContext(DatasetEntry):
+class DateContext(Context):
     date: str = None
 
     @classmethod
-    def from_context(cls, context: DatasetEntry, date: str) -> "DateContext":
+    def from_context(cls, context: Context, date: str) -> "DateContext":
         return cls(
             **context.model_dump(),
             date=date,
