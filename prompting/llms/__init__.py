c42182644c4d454299bb0b0c758c00d8b81c241d
richwardle <richardwardle@macrocosmos.ai>
2024-11-18 04:33:19 -1000

    remove unused vllm pipeline

--- prompting/llms/__init__.py
+++
@@ -1,2 +0,0 @@
-# from .base_llm import BasePipeline as BasePipeline, BaseLLM as BaseLLM
-# from .vllm_llm import vLLM_LLM as vLLM_LLM, vLLMPipeline as vLLMPipeline, load_vllm_pipeline as load_vllm_pipeline
