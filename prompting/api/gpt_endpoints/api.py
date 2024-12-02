49a32249765c9c1c95ce81d0e175f4df724ad222
Hollyqui <felix.quinque@gmail.com>
2024-12-02 10:58:15 +0000

    Fixing formatting

--- prompting/api/gpt_endpoints/api.py
+++ prompting/api/gpt_endpoints/api.py
@@ -2,22 +2,18 @@ import json
 import random
 from typing import AsyncGenerator
 
-import openai
 from fastapi import APIRouter, Depends, HTTPException, Request
-from fastapi.responses import StreamingResponse
-from httpx import Timeout
 from loguru import logger
 
 from prompting.api.api_managements.api import validate_api_key
 from prompting.base.dendrite import DendriteResponseEvent, SynapseStreamResult
-from prompting.base.epistula import create_header_hook, query_miners
+from prompting.base.epistula import query_miners
 from prompting.miner_availability.miner_availability import miner_availabilities
 from prompting.rewards.scoring import task_scorer
 from prompting.settings import settings
 from prompting.tasks.inference import InferenceTask
 from prompting.tasks.task_registry import TaskRegistry
 from prompting.utils.timer import Timer
-
 
 router = APIRouter()
 
@@ -75,7 +71,7 @@ async def proxy_chat_completions(request: Request, api_key_data: dict = Depends(
 
     stream = body.get("stream")
     body = {k: v for k, v in body.items() if k not in ["task", "stream"]}
-    body['task'] = task.__class__.__name__
+    body["task"] = task.__class__.__name__
     body["seed"] = body.get("seed") or str(random.randint(0, 1_000_000))
     logger.debug(f"Seed provided by miner: {bool(body.get('seed'))} -- Using seed: {body.get('seed')}")
 
@@ -85,19 +81,20 @@ async def proxy_chat_completions(request: Request, api_key_data: dict = Depends(
         available_miners := miner_availabilities.get_available_miners(task=task, model=body.get("model"))
     ):
         raise HTTPException(
-            status_code=503, detail=f"No miners available for model: {body.get('model')} and task: {task.__class__.__name__}"
+            status_code=503,
+            detail=f"No miners available for model: {body.get('model')} and task: {task.__class__.__name__}",
         )
 
-    response = query_miners(available_miners, json.dumps(body).encode("utf-8"), stream = stream)
+    response = query_miners(available_miners, json.dumps(body).encode("utf-8"), stream=stream)
     if stream:
         return response
     else:
         response = await response
         response_event = DendriteResponseEvent(
-            stream_results = response,
-            uids = available_miners,
-            timeout = settings.NEURON_TIMEOUT,
-            completions = ["".join(res.accumulated_chunks) for res in response]
+            stream_results=response,
+            uids=available_miners,
+            timeout=settings.NEURON_TIMEOUT,
+            completions=["".join(res.accumulated_chunks) for res in response],
         )
 
         task = task(
@@ -109,11 +106,15 @@ async def proxy_chat_completions(request: Request, api_key_data: dict = Depends(
         )
 
         task_scorer.add_to_queue(
-            task=task, response=response_event, dataset_entry=task.dataset_entry, block=-1, step=-1, task_id=task.task_id
+            task=task,
+            response=response_event,
+            dataset_entry=task.dataset_entry,
+            block=-1,
+            step=-1,
+            task_id=task.task_id,
         )
-        
-        return response
 
+        return response
 
     # axon_info = settings.METAGRAPH.axons[available_miners[0]]
     # base_url = "http://localhost:8008/v1" if settings.mode == "mock" else f"http://{axon_info.ip}:{axon_info.port}/v1"
