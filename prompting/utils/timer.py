2f50da2d912989b825837732ee1e1e77a6972384
bkb2135 <98138173+bkb2135@users.noreply.github.com>
2024-12-02 06:41:52 -0500

    API Release (#469)
    Co-authored-by: richwardle <richardwardle@macrocosmos.ai>
    Co-authored-by: Hollyqui <felix.quinque@gmail.com>
    Co-authored-by: richwardle <richard.wardle@macrocosmos.ai>
    Co-authored-by: Dmytro Bobrenko <17252809+dbobrenko@users.noreply.github.com>

--- prompting/utils/timer.py
+++ prompting/utils/timer.py
@@ -6,6 +6,9 @@ class Timer:
         self.start_time = time.perf_counter()
         return self
 
+    def elapsed_time(self):
+        return self.start_time - time.perf_counter()
+
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.end_time = time.perf_counter()
-        self.elapsed_time = self.end_time - self.start_time
+        self.final_time = self.end_time - self.start_time
