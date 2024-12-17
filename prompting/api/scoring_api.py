61a71eb3b01a28b3f7c8bc06df735e63f38821bc
bkb2135 <bkb2135@columbia.edu>
2024-12-17 03:25:07 +0000

    Formatting

--- prompting/api/scoring_api.py
+++ prompting/api/scoring_api.py
@@ -1,16 +1,16 @@
 import uvicorn
-from fastapi import FastAPI, Request, HTTPException, Header, Depends
+from fastapi import Depends, FastAPI, Header, HTTPException, Request
 from loguru import logger
 
-
-from prompting.settings import settings
 from prompting import mutable_globals
-from prompting.tasks.inference import InferenceTask
-from prompting.rewards.scoring import ScoringConfig
 from prompting.datasets.base import DatasetEntry
+from prompting.rewards.scoring import ScoringConfig
+from prompting.settings import settings
+from prompting.tasks.inference import InferenceTask
 from shared.dendrite import DendriteResponseEvent, SynapseStreamResult
 
 app = FastAPI()
+
 
 def verify_api_key(authorization: str = Header(None)):
     """
@@ -27,34 +27,38 @@ def verify_api_key(authorization: str = Header(None)):
 
     return True
 
+
 @app.post("/scoring")
-async def scoring_endpoint(
-    request: Request, 
-    verified: bool = Depends(verify_api_key)
-):
+async def scoring_endpoint(request: Request, verified: bool = Depends(verify_api_key)):
     try:
         payload = await request.json()
         logger.debug(f"Received scoring request: {request}")
-        
+
         body = payload.get("body")
         if not body:
             logger.error("Bad Request: 'scoring_info' is missing from payload")
             raise HTTPException(status_code=400, detail="body (the original request) is required in payload")
-        
+
         if body.get("task") != "InferenceTask":
-            logger.warning("A non inference task has been received, which is not supported for organic scoring. Skipping...")
+            logger.warning(
+                "A non inference task has been received, which is not supported for organic scoring. Skipping..."
+            )
         else:
             try:
                 # Create a task w/reference
-                task = InferenceTask(query = body.get("messages")[-1].get("content"), seed = body.get("seed"), 
-                                    sampling_params=body.get("sampling_parameters"), llm_model=body.get("model"))
+                task = InferenceTask(
+                    query=body.get("messages")[-1].get("content"),
+                    seed=body.get("seed"),
+                    sampling_params=body.get("sampling_parameters"),
+                    llm_model=body.get("model"),
+                )
                 task.generate_reference()
                 mutable_globals.scoring_queue.append(
                     ScoringConfig(
-                        task = task, 
-                        response = DendriteResponseEvent(
+                        task=task,
+                        response=DendriteResponseEvent(
                             uids=payload.get("uid"),
-                            timeout = settings.NEURON_TIMEOUT, 
+                            timeout=settings.NEURON_TIMEOUT,
                             stream_results=[
                                 SynapseStreamResult(
                                     accumulated_chunks=payload.get("chunks"),
@@ -65,11 +69,11 @@ async def scoring_endpoint(
                                     status_message="success" if payload.get("chunks") else "errored",
                                 )
                             ],
-                            #TODO: MAY HAVE TO ADD THE REST OF THE DENDRITE RESPONSE EVENT FIELDS
-                        ), 
+                            # TODO: MAY HAVE TO ADD THE REST OF THE DENDRITE RESPONSE EVENT FIELDS
+                        ),
                         dataset_entry=DatasetEntry(),
-                        block = settings.METAGRAPH.block,
-                        step = 99999,
+                        block=settings.METAGRAPH.block,
+                        step=99999,
                         task_id="organic",
                     )
                 )
@@ -83,7 +87,9 @@ async def scoring_endpoint(
 
 async def start_scoring_endpoint():
     if not settings.SCORING_ENDPOINT_HOST and settings.SCORING_ENDPOINT_PORT:
-        logger.exception("If you are deploying an api you must set SCORING_ENDPOINT_HOST and SCORING_ENDPOINT_PORT in the .env.validator")
+        logger.exception(
+            "If you are deploying an api you must set SCORING_ENDPOINT_HOST and SCORING_ENDPOINT_PORT in the .env.validator"
+        )
         raise RuntimeError
     uvicorn.run(
         app,
