7a06030207a1be4a1b8f3e06a28b2328cc4c7861
Hollyqui <felix.quinque@gmail.com>
2025-01-20 13:23:48 +0000

    Adding draft for time penalty

--- prompting/rewards/exact_match.py
+++ prompting/rewards/exact_match.py
@@ -3,22 +3,33 @@ import numpy as np
 from prompting.rewards.reward import BaseRewardModel, BatchRewardOutput
 from shared.dendrite import DendriteResponseEvent
 
-PENALTY_FACTOR = 0
+PENALTY_FACTOR = 3
 
 
 class ExactMatchRewardModel(BaseRewardModel):
     def reward(self, reference: str, response_event: DendriteResponseEvent, **kwargs) -> BatchRewardOutput:
         """Gives an exact reward of 1 if the response matches the reference, 0 otherwise"""
         rewards = []
-        completions: list[str] = response_event.completions
-        timings = [0] * len(completions)
+        # completions: list[str] = response_event.completions
+        all_chunks: list[list[str]] = response_event.stream_results_all_chunks
+        all_timings: list[list[float]] = response_event.stream_results_all_chunks_timings
 
-        for completion in completions:
-            rewards.append(1 if reference == completion else -PENALTY_FACTOR)
+        for chunks, timings in zip(all_chunks, all_timings):
+            completion = "".join(chunks)
+            if reference != completion:
+                rewards.append(-PENALTY_FACTOR)
+                continue
+
+            miner_reward = 0
+            for chunk, timing in zip(chunks, timings):
+                if chunk == "":
+                    miner_reward = 0
+                miner_reward += 1 - min(1, max(0, (timing / response_event.timeout)))
+            rewards.append(miner_reward / len(chunks))
 
         output = BatchRewardOutput(
             rewards=np.array(rewards),
-            timings=np.array(timings),
+            timings=np.array(all_timings),
         )
 
         return output
