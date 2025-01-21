5f314d19b72fd84811bd921c67d534f1c7571f12
richwardle <richard.wardle@macrocosmos.ai>
2025-01-21 14:37:56 +0000

    add debug statement

--- prompting/rewards/exact_match.py
+++ prompting/rewards/exact_match.py
@@ -1,4 +1,5 @@
 import numpy as np
+from loguru import logger
 
 from prompting.rewards.reward import BaseRewardModel, BatchRewardOutput
 from shared.dendrite import DendriteResponseEvent
@@ -8,23 +9,48 @@ PENALTY_FACTOR = 3
 
 class ExactMatchRewardModel(BaseRewardModel):
     def reward(self, reference: str, response_event: DendriteResponseEvent, **kwargs) -> BatchRewardOutput:
-        """Gives an exact reward of 1 if the response matches the reference, 0 otherwise"""
+        """
+        Calculates rewards based on the exact match of the response with the reference string.
+
+        - If the response matches the reference:
+            - Rewards are calculated based on timing metrics for each chunk.
+            - Slower chunks receive reduced rewards
+        - If the response does not match the reference, a penalty is applied.
+
+        Parameters:
+            reference (str): The expected response string.
+            response_event (DendriteResponseEvent): Contains completions, timings, and other details.
+
+        Returns:
+            BatchRewardOutput: An object containing the computed rewards and timing details.
+        """
         rewards = []
-        # completions: list[str] = response_event.completions
         all_chunks: list[list[str]] = response_event.stream_results_all_chunks
-        all_timings: list[list[float]] = response_event.stream_results_all_chunks_timings
+        all_timings: list[list[float]] = response_event.stream_results_all_chunks_timing
+        timeout = response_event.timeouts
+
+        debug_output = {
+            # "reference": reference,
+            "all_chunks": all_chunks,
+            "all_timings": all_timings,
+            "timeout": timeout,
+        }
+
+        logger.error(str(debug_output) + "£££££")
 
         for chunks, timings in zip(all_chunks, all_timings):
             completion = "".join(chunks)
             if reference != completion:
+                # Apply penalty if there is no exact match
                 rewards.append(-PENALTY_FACTOR)
                 continue
 
             miner_reward = 0
             for chunk, timing in zip(chunks, timings):
-                if chunk == "":
-                    miner_reward = 0
-                miner_reward += 1 - min(1, max(0, (timing / response_event.timeout)))
+                if chunk:
+                    normalized_timing = min(1, max(0, (timing / timeout)))
+                    miner_reward += normalized_timing
+
             rewards.append(miner_reward / len(chunks))
 
         output = BatchRewardOutput(
