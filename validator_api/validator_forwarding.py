6961c5ef2e947bd0ceed34d5710ecb31b9bd7a4a
richwardle <richard.wardle@macrocosmos.ai>
2025-02-01 08:21:00 +0000

    Intermediary Dump

--- validator_api/validator_forwarding.py
+++ validator_api/validator_forwarding.py
@@ -1,4 +1,28 @@
 from shared.settings import shared_settings
+from pydantic import BaseModel, model_validator
+from shared.misc import cached_property_with_expiration
+
+class ValidatorRegistry(BaseModel):
+    """
+    This is a class to store the success of forwards to validator axons.
+    If a validator is routinely failing to respond to scoring requests we should stop sending them to them. 
+    """
+
+    availability_registry: dict | None = None
+
+    
+
+    @cached_property_with_expiration(expiration_seconds=12000)
+    def AXON_STAKE_DICT(self, metagraph = shared_settings.METAGRAPH) -> dict:
+        validator_uids = [uid for uid in metagraph.validator_permit if metagraph.validator_permit[uid]]
+        validator_axons = [metagraph.axons[uid].ip_str().split("/") for uid in validator_uids]
+        validator_stakes = [metagraph.stake[uid] for uid in validator_uids]
+        axon_stake_dict: dict = {axon: stake for axon, stake in zip(validator_axons, validator_stakes)}
+
+
+    def get_availabilities(self):
+
+
 
 class ValidatorForwarding:
     """
