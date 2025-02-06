31bf1b54ba004e23e87d248496e9d75453b5a3ac
xfu <angerpeanut@gmail.com>
2025-02-06 17:32:26 +0800

    AttributeError: 'SharedSettings' object has no attribute 'INFERENCE_TIMEOUT' (#567)
    1. SharedSettings has no INFERENCE_TIMEOUT
    2. Fields of website should be optional none

--- shared/settings.py
+++ shared/settings.py
@@ -55,6 +55,7 @@ class SharedSettings(BaseSettings):
 
     # Neuron parameters.
     NEURON_TIMEOUT: int = Field(20, env="NEURON_TIMEOUT")
+    INFERENCE_TIMEOUT: int = Field(60, env="INFERENCE_TIMEOUT")
     NEURON_DISABLE_SET_WEIGHTS: bool = Field(False, env="NEURON_DISABLE_SET_WEIGHTS")
     NEURON_MOVING_AVERAGE_ALPHA: float = Field(0.1, env="NEURON_MOVING_AVERAGE_ALPHA")
     NEURON_DECAY_ALPHA: float = Field(0.001, env="NEURON_DECAY_ALPHA")
