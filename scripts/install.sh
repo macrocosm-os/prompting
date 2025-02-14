a7dc199be99431792bf228c6d29e084e382e7d4e
bkb2135 <98138173+bkb2135@users.noreply.github.com>
2025-02-14 14:34:57 -0500

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,7 +12,7 @@ poetry install --extras "validator"
 # Build AutoAWQ==0.2.8 from source
 if [ -d AutoAWQ ]; then rm -rf AutoAWQ; fi
 git clone https://github.com/casper-hansen/AutoAWQ.git
-cd AutoAWQ  && git checkout 16335d087dd4f9cdc8933dd7a5681e4bf88311b6 && poetry run pip install -e . && cd ..
+cd AutoAWQ && git checkout 16335d087dd4f9cdc8933dd7a5681e4bf88311b6 && poetry run pip install -e . && cd ..
 
 poetry run pip install flash-attn --no-build-isolation
 
