b696a2c2859005fac8cd011204735f3dd72395dd
Hollyqui <felix.quinque@gmail.com>
2025-03-26 17:33:34 +0000

    hardcording miner port

--- neurons/miners/epistula_miner/miner.py
+++ neurons/miners/epistula_miner/miner.py
@@ -160,11 +160,21 @@ class OpenAIMiner:
                 return
 
             # Stream tokens and their logprobs
+<<<<<<< Updated upstream
             for step in result["token_logprobs"]:
                 logger.info(step)
                 token = step["token"]
                 logprobs_info = {"top_tokens": step["top_tokens"], "top_logprobs": step["top_logprobs"]}
 
+=======
+            for step in result['token_logprobs']:
+                token = step['token']
+                logprobs_info = {
+                    'top_tokens': step['top_tokens'],
+                    'top_logprobs': step['top_logprobs']
+                }
+                
+>>>>>>> Stashed changes
                 # Format in OpenAI streaming style but include logprobs
                 data = {
                     "choices": [
@@ -199,7 +209,7 @@ class OpenAIMiner:
 
         # Set all model availabilities to False (openai will not be able to handle seeded inference)
         model_response = {key: key == LOCAL_MODEL_ID for key in llm_model_availabilities}
-
+        print(model_response)
         response = {"task_availabilities": task_response, "llm_model_availabilities": model_response}
 
         return response
@@ -247,14 +257,14 @@ class OpenAIMiner:
                 logger.error("Failed to get external IP")
 
         logger.info(
-            f"Serving miner endpoint {external_ip}:{shared_settings.AXON_PORT} on network: {shared_settings.SUBTENSOR_NETWORK} with netuid: {shared_settings.NETUID}"
+            f"Serving miner endpoint {external_ip}:{11175} on network: {shared_settings.SUBTENSOR_NETWORK} with netuid: {shared_settings.NETUID}"
         )
 
         serve_success = serve_extrinsic(
             subtensor=shared_settings.SUBTENSOR,
             wallet=shared_settings.WALLET,
             ip=external_ip,
-            port=shared_settings.AXON_PORT,
+            port=11175,
             protocol=4,
             netuid=shared_settings.NETUID,
         )
@@ -279,7 +289,8 @@ class OpenAIMiner:
         fast_config = uvicorn.Config(
             app,
             host="0.0.0.0",
-            port=shared_settings.AXON_PORT,
+            # port=shared_settings.AXON_PORT,
+            port=11175,
             log_level="info",
             loop="asyncio",
             workers=4,
