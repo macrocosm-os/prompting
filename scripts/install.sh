665467d6d6d7ea7c9b0c5a37fb3df20f27891c9e
bkb2135 <bkb2135@columbia.edu>
2025-04-10 02:10:51 -0800

    Pin VLLM version

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,7 +8,7 @@ poetry config virtualenvs.in-project true
 
 # Install the project dependencies
 poetry install --extras "validator" --no-cache
-poetry run pip install vllm
+poetry run pip install vllm==0.8.3
 
 # Check if jq is installed and install it if not
 if ! command -v jq &> /dev/null
